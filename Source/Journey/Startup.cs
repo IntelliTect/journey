de0a67b0ddb6fb77baeefe9893d2dea279b94949
Grant Erickson <grant@granterickson.com>
2015-08-26 11:52:24 -0700

    Initial Project Creation.

---
+++ Source/Journey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Journey.Startup))]
+namespace Journey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
