568f478c969ab5d3ad1ba88dd9356c0312830931
Grant Erickson <grant@granterickson.com>
2015-08-28 15:43:20 -0700

    Changed journey calls to methods. Minor content update.

--- Source/Journey/IntelliTect/Journey.cs
+++ Source/Journey/IntelliTect/Journey.cs
@@ -9,35 +9,26 @@ namespace IntelliTect
 {
     public class Journey
     {
-        public static ActionResult Close
+        public static ActionResult Close()
         {
-            get
+            return new ContentResult()
             {
-                return new ContentResult()
-                {
-                    Content = "Journey Page Close"
-                };
-            }
+                Content = "Journey Page Close"
+            };
         }
-        public static ActionResult RefreshPage
+        public static ActionResult RefreshPage()
         {
-            get
+            return new ContentResult()
             {
-                return new ContentResult()
-                {
-                    Content = "Journey Refresh Page"
-                };
-            }
+                Content = "Journey Refresh Page"
+            };
         }
-        public static ActionResult RefreshAll
+        public static ActionResult RefreshAll()
         {
-            get
+            return new ContentResult()
             {
-                return new ContentResult()
-                {
-                    Content = "Journey Refresh All"
-                };
-            }
+                Content = "Journey Refresh All"
+            };
         }
 
 
